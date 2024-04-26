eab010f4ee83fb229221e8b282e10c19b3ba668d
Peter Cyprich <cypooriginal@gmail.com>
2024-04-26 17:23:12 +0200

    obrazky cez after effects

--- src/entity/zombies/Zombie.java
+++ src/entity/zombies/Zombie.java
@@ -2,6 +2,6 @@ package entity.zombies;
 
 public class Zombie extends entity.Entita {
     public Zombie(int x, int y) {
-        super(x, y, "zombies/zombie", 120);
+        super(x, y, "zombies/zombie", 240);
     }
 }
